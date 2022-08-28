4236187603ae9113362f94213f23c37489f6cadd
Rods <rodolfo.perez01@iest.edu.mx>
2022-08-28 15:51:31 -0500

    Autorización

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors = require("cors")
 
 
+const routerAuth = require("./routes/auth.route")
 const routerUser = require("./routes/user.route")
 const routerPost = require("./routes/post.route")
 
@@ -11,5 +12,6 @@ app.use(express.json())
 
 app.use("/user", routerUser)
 app.use("/post", routerPost)
+app.use("/auth", routerAuth)
 
 module.exports= app
